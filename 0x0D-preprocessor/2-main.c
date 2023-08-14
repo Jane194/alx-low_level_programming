1319d103ec958bcd006b7fb83e229c52e51c756f
Jane194 <janetaklah@gmail.com>
2023-08-14 05:30:16 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-* main - prints the name of the file
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints the name of the file of the program
+ * was comiled from, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int ain(void)
 {
-       printf("%s\n", __FILE__);
-       return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
